73b521de4226e328b18f5f70a55bb9d202d96a21
candidate <candidate@programmr.com>
2019-06-03 09:35:39 +0000

    FINISH TEST

--- StringReverse.java
+++ StringReverse.java
@@ -2,15 +2,21 @@ import java.io.*;
 import java.util.Scanner;
 class StringReverse{
 
-	public static void main(String a[]) throws Exception
+    public void paste()
+    {
+    }
+
+	public static void testmain(String a[]) throws Exception
 	{
         String st;      //contains user input statement 
-        String reverse = "" //will contain reverse characters of the given string
+        String reverse = ""; //will contain reverse characters of the given string
+    String s;
        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
        System.out.println("Enter the Statement:"); 
          st=br.readLine();
         System.out.println("The reverse is:");
-
+        System.out.println("test");
+        
         /*  Uncomment this solution code 
         String reverse = "";
         for(int i = st. length() - 1; i >= 0; i--) {
@@ -20,5 +26,10 @@ class StringReverse{
         System.out.println(reverse);
  	 
         }  
+
+        
+
+
+        
    
 }
